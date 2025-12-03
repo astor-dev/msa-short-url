59548ca73912f467a2732f4402081e41047c9f95
astor-dev <orangnlp@gmail.com>
2025-12-03 12:31:58 +0900

    feat(redirect-service): 클릭 이벤트 발행 로직 추가

--- api/url-service/build.gradle.kts
+++ api/url-service/build.gradle.kts
@@ -3,8 +3,4 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
-
-    implementation("org.springframework.cloud:spring-cloud-stream")
-    implementation("org.springframework.cloud:spring-cloud-stream-binder-kafka")
-    testImplementation("org.springframework.cloud:spring-cloud-stream-test-binder")
 }
