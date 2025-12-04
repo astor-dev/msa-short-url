8d3309d6fb10fb19dce5809e938485d15255d5bf
astor-dev <orangnlp@gmail.com>
2025-12-04 13:01:36 +0900

    feat(short-url-stats): short-url-total-stats service 구현

---
+++ domain/short-url-stats/src/main/kotlin/com/naver/pay/shorturl/stats/ShortUrlTotalStatsService.kt
@@ -0,0 +1,28 @@
+package com.naver.pay.shorturl.stats
+
+import com.naver.pay.shorturl.stats.infrastructure.mongodb.ShortUrlTotalStatsDocument
+import com.naver.pay.shorturl.stats.infrastructure.mongodb.ShortUrlTotalStatsRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class ShortUrlTotalStatsService(
+    private val shortUrlTotalStatsRepository: ShortUrlTotalStatsRepository
+) {
+
+    fun createTotalStats(shortKey: String, metadata: ShortUrlMetadata) {
+        val document = ShortUrlTotalStatsDocument(
+            shortKey = shortKey,
+            metadata = metadata
+        )
+        shortUrlTotalStatsRepository.save(document)
+    }
+
+    fun click(shortKey: String, referrer: String, device: String, date: String) {
+        shortUrlTotalStatsRepository.recordClickAtomically(shortKey, referrer, device, date)
+    }
+
+    fun findOne(shortKey: String): ShortUrlTotalStatsDocument? {
+        return shortUrlTotalStatsRepository.findByIdOrNull(shortKey)
+    }
+}
--- domain/short-url-stats/src/main/kotlin/com/naver/pay/shorturl/stats/infrastructure/mongodb/ShortUrlTotalStatsDocument.kt
+++ domain/short-url-stats/src/main/kotlin/com/naver/pay/shorturl/stats/infrastructure/mongodb/ShortUrlTotalStatsDocument.kt
@@ -13,12 +13,12 @@ import java.time.Instant
 class ShortUrlTotalStatsDocument (
     @Id
     val shortKey: String,
-    val totalClicks: Long,
+    val totalClicks: Long = 0,
     // NOTE: yyyy-MM-dd
     val byDate: MutableMap<String, Long> = mutableMapOf(),
     val byDevice: MutableMap<String, Long> = mutableMapOf(),
     val byReferrer: MutableMap<String, Long> = mutableMapOf(),
-    val lastClickedAt: Instant?,
+    val lastClickedAt: Instant? = null,
     val metadata: ShortUrlMetadata,
 ) {
     fun toDomain(): ShortUrlTotalStats {
